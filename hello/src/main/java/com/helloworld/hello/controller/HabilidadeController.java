5acfa51086c3e2fc3a50dafcc9ee54c31d624b5a
tainah vinha <tainah_vinha@hotmail.com>
2021-05-25 15:58:20 -0300

    Hello World no Spring

---
+++ hello/src/main/java/com/helloworld/hello/controller/HabilidadeController.java
@@ -0,0 +1,13 @@
+package com.helloworld.hello.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/habilidades")
+
+public class HabilidadeController {
+	
+	
+
+}
